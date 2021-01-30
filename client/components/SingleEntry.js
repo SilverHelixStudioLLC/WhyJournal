c89cf8e6b8b9beb9710906167093264545087800
Jasmine Wang <jasminey.tea@gmail.com>
2021-01-30 13:11:23 -0600

    adds single entry component

---
+++ client/components/SingleEntry.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getSingleEntryThunk } from '../store';
+
+export class SingleEntry extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const userId = this.props.userId;
+    const entryId = this.props.match.params.entryId;
+    this.props.getEntry(userId, entryId);
+  }
+
+  render() {
+    console.log('thissss', this.props.entry)
+    const title = this.props.entry.title;
+    const content = this.props.entry.content;
+    const updatedAt = this.props.entry.updatedAt;
+    return <div>
+      <h1>{title}</h1>
+      <h2>{updatedAt}</h2>
+      <p>{content}</p>
+    </div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    userId: state.user.me.id,
+    entry: state.entry.single,
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getEntry(userId, entryId) {dispatch(getSingleEntryThunk(userId, entryId))},
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleEntry)
+
+SingleEntry.propTypes = {
+
+}
