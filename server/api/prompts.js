54e98794d60fa07cbb3459488c52d7b98ba297f2
Joncitone <jarreola7123@gmail.com>
2021-02-03 16:33:00 -0600

    added middlware for sensitive routes

--- server/api/prompts.js
+++ server/api/prompts.js
@@ -1,56 +1,57 @@
-const router = require('express').Router();
-const { Prompt } = require('../db/models');
-module.exports = router;
+const router = require('express').Router()
+const {Prompt} = require('../db/models')
+const {adminMiddleware} = require('./middleware')
+
+module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const prompts = await Prompt.findAll();
-    prompts ? res.json(prompts) : res.status(400).end();
+    const prompts = await Prompt.findAll()
+    prompts ? res.json(prompts) : res.status(400).end()
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
 router.get('/:promptId', async (req, res, next) => {
   try {
-    // console.log(req.params.promptId)
-    const prompt = await Prompt.findByPk(req.params.promptId);
-    prompt ? res.json(prompt) : res.status(400).end();
+    const prompt = await Prompt.findByPk(req.params.promptId)
+    prompt ? res.json(prompt) : res.status(400).end()
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
 // admin only actions below
-router.post('/', async (req, res, next) => {
+router.post('/', adminMiddleware, async (req, res, next) => {
   try {
-    const newPrompt = await Prompt.create(req.body);
-    newPrompt ? res.json(newPrompt) : res.status(400).end();
+    const newPrompt = await Prompt.create(req.body)
+    newPrompt ? res.json(newPrompt) : res.status(400).end()
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-router.put('/:promptId', async (req, res, next) => {
+router.put('/:promptId', adminMiddleware, async (req, res, next) => {
   try {
-    const [numUpdated, updatedPrompts] = await Prompt.update(req.body, {
-      where: { id: req.params.promptId },
+    const [, updatedPrompts] = await Prompt.update(req.body, {
+      where: {id: req.params.promptId},
       returning: true,
-      plain: true,
-    });
-    updatedPrompts ? res.json(updatedPrompts[0]) : res.status(400).end();
+      plain: true
+    })
+    updatedPrompts ? res.json(updatedPrompts[0]) : res.status(400).end()
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-router.delete('/:promptId', async (req, res, next) => {
+router.delete('/:promptId', adminMiddleware, async (req, res, next) => {
   try {
     const promptDeleted = await Prompt.destroy({
-      where: { id: req.params.promptId },
-    });
-    promptDeleted ? res.send('prompt deleted') : res.send('deletion failed');
+      where: {id: req.params.promptId}
+    })
+    promptDeleted ? res.send('prompt deleted') : res.send('deletion failed')
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
--- server/api/users.js
+++ server/api/users.js
@@ -1,60 +1,94 @@
-const router = require('express').Router();
-const { User } = require('../db/models');
-module.exports = router;
+const router = require('express').Router()
+const {User} = require('../db/models')
+const {adminMiddleware, isUserOrAdminMiddleware} = require('./middleware')
 
-router.get('/', async (req, res, next) => {
+module.exports = router
+
+router.get('/', adminMiddleware, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email'],
-    });
-    res.json(users);
+      attributes: ['id', 'email']
+    })
+    res.json(users)
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-router.get('/:userId', async (req, res, next) => {
+router.get('/:userId', isUserOrAdminMiddleware, async (req, res, next) => {
   try {
-    // console.log(req.params.userId)
-    const user = await User.findByPk(req.params.userId);
-    user ? res.json(user) : res.status(400).end();
+    const user = await User.findByPk(req.params.userId)
+    user ? res.json(user) : res.status(400).end()
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
 router.post('/', async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
-    newUser ? res.json(newUser) : res.status(400).end();
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      googleId,
+      emailOptIn
+    } = req.body
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+      googleId,
+      emailOptIn
+    })
+    newUser ? res.json(newUser) : res.status(400).end()
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-router.put('/:userId', async (req, res, next) => {
+router.put('/:userId', isUserOrAdminMiddleware, async (req, res, next) => {
   try {
-    const [, updatedUsers] = await User.update(req.body, {
-      where: { id: req.params.userId },
-      returning: true,
-      plain: true,
-    });
-    updatedUsers ? res.json(updatedUsers[0]) : res.status(400).end();
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      googleId,
+      emailOptIn
+    } = req.body
+    const [, updatedUsers] = await User.update(
+      {
+        firstName,
+        lastName,
+        email,
+        password,
+        googleId,
+        emailOptIn
+      },
+      {
+        where: {id: req.params.userId},
+        returning: true,
+        plain: true
+      }
+    )
+    updatedUsers ? res.json(updatedUsers[0]) : res.status(400).end()
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-router.delete('/:userId', async (req, res, next) => {
+router.delete('/:userId', isUserOrAdminMiddleware, async (req, res, next) => {
   try {
     const userDeleted = await User.destroy({
-      where: { id: req.params.userId },
-    });
-    userDeleted ? res.send('user deleted') : res.send('deletion failed');
+      where: {id: req.params.userId}
+    })
+    userDeleted ? res.send('user deleted') : res.send('deletion failed')
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
