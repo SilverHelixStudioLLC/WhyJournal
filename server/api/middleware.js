c6590a3fd4525e63b9d0c998024bc578c88ab980
Joncitone <jarreola7123@gmail.com>
2021-02-03 16:49:50 -0600

    fixed middleware by changing req.user to req.session.user

--- server/api/middleware.js
+++ server/api/middleware.js
@@ -1,5 +1,5 @@
 const adminMiddleware = (req, res, next) => {
-  if (!req.user.isAdmin) {
+  if (!req.session.user.isAdmin) {
     const err = new Error('Forbidden, request requires elevated permissions')
     err.status = 401
 
@@ -9,7 +9,7 @@ const adminMiddleware = (req, res, next) => {
 }
 
 const userCheckMiddleware = (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (req.session.user && req.session.user.id !== req.params.userId) {
     const err = new Error('Forbidden, request denied, invalid user')
     err.status = 401
 
@@ -19,7 +19,7 @@ const userCheckMiddleware = (req, res, next) => {
 }
 
 const isUserOrAdminMiddleware = (req, res, next) => {
-  if (req.user.id === req.params.userId || req.user.isAdmin) {
+  if (req.session.user.id === req.params.userId || req.session.user.isAdmin) {
     next()
   } else {
     const err = new Error('Forbidden, request denied, invalid credentials')
