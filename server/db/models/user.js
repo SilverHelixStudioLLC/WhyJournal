c681edf1901d1b2a5479730a1880ea49345448fb
Joncitone <jarreola7123@gmail.com>
2021-02-05 17:11:59 -0600

    adjusted user model field spacing

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
-  firstName: {type: Sequelize.STRING, allowNull: false},
-  lastName: {type: Sequelize.STRING},
+  firstName: { type: Sequelize.STRING, allowNull: false },
+  lastName: { type: Sequelize.STRING },
   email: {
     type: Sequelize.STRING,
     unique: true,
@@ -12,22 +12,16 @@ const User = db.define('user', {
   },
   password: {
     type: Sequelize.STRING,
-    // Making `.password` act like a func hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('password')
     }
   },
-
   salt: {
     type: Sequelize.STRING,
-    // Making `.salt` act like a function hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('salt')
     }
   },
-
   googleId: {
     type: Sequelize.STRING
   },
